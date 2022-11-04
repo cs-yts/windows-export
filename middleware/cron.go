98c8ea324fbd94b3c176e7f8c1c5ad0157b93b36
cs-yts <yts1115084429@163.com>
2022-11-04 17:11:58 +0800

    更新定时任务

--- middleware/cron.go
+++ middleware/cron.go
@@ -12,30 +12,30 @@ func Cron() {
 	c := cron.New(cron.WithSeconds())
 	//添加定时任务
 	//获取cpu
-	// _, err := c.AddFunc("*/10 * * * * *", model.GetCpuInfo)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//获取内存
-	// _, err = c.AddFunc("*/10 * * * * *", model.GetMemInfo)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//获取磁盘实例
-	// _, err = c.AddFunc("*/10 * * * * *", model.GetDiskInfo)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//获取进程
-	_, err := c.AddFunc("*/10 * * * * *", model.GetProcess)
+	_, err := c.AddFunc("*/10 * * * * *", model.GetCpuInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//获取负载
-	// _, err = c.AddFunc("*/10 * * * * *", model.GetSysLoad)
+	//获取内存
+	_, err = c.AddFunc("*/10 * * * * *", model.GetMemInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//获取磁盘实例
+	_, err = c.AddFunc("*/10 * * * * *", model.GetDiskInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//获取进程
+	// _, err := c.AddFunc("*/10 * * * * *", model.GetProcess)
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
+	//获取负载
+	_, err = c.AddFunc("*/10 * * * * *", model.GetSysLoad)
+	if err != nil {
+		log.Fatal(err)
+	}
 	c.Start()
 	select {}
 }
